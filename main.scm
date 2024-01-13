edecad05ee99a98a597f7cc75050723db0e2aef0
t-matsudate <41981585+t-matsudate@users.noreply.github.com>
2024-01-13 11:52:52 +0000

    ABC081A追加

--- main.scm
+++ main.scm
@@ -6,4 +6,11 @@
     (if (odd? (* a b))
         (print "Odd")
         (print "Even"))))
-(abc086a)
+
+(define (abc081a)
+  (let* ((line (read-line (current-input-port)))
+         (nums (map digit->integer (string->list line))))
+    (print (length (filter (lambda (x) (= x 1)) nums)))))
+
+;(abc086a)
+(abc081a)
